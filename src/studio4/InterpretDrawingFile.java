3f5ec747919b326008c6592974287c4df7d4f71b
jordanalikhan <jordanalikhan@localhost>
2024-10-03 10:51:07 -0500

    push

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,57 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next();
+		
+		int redComponent = in.nextInt();
+		
+		int greenComponent = in.nextInt();
+		
+		int blueComponent = in.nextInt(); 
+		
+		boolean isFilled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+	
+		double y = in.nextDouble();
+		
+		double halfWidth = in.nextDouble();
+		
+		double halfHeight = in.nextDouble();
+		
+		double trianglex = in.nextDouble();
+		
+		double triangley = in.nextDouble();
+		
+
+		
+		Color machine = new Color (redComponent, greenComponent, blueComponent);
+		
+		StdDraw.setPenColor(machine);
+			if (shapeType.equals("rectangle")) {
+				if (isFilled==true) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+				}
+				else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+				}
+			}
+			if (shapeType.equals("ellipse")) {
+				if(isFilled==true) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+				}
+				else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+				}
+			}
+			
+			double []shook = {x, y, halfWidth};
+			
+			double [] doug = {halfHeight, trianglex, triangley};
+			
+			StdDraw.polygon(shook, doug);
+			
 		
 	}
 }
