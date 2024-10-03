8940c612f70a7292edb65bd85e2de52dc2690d22
jordanalikhan <jordanalikhan@localhost>
2024-10-03 11:14:04 -0500

    done!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledRectangle(0.5,0.5,0.4,0.25);
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledEllipse(0.55, 0.5, 0.3, 0.15);
+		double []x =  {0.25, 0.55, 0.5};
+		double [] y = {0.25, 0.25, 0.55};
+		StdDraw.filledPolygon(x, y);
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
